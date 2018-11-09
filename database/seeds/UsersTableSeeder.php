783344f64e867c2fa1fd008165932e6e63c89487
luolei <luolei1116@gmail.com>
2018-11-09 09:35:16 +0000

    Add account activations

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,11 +15,12 @@ class UsersTableSeeder extends Seeder
         $users = factory(User::class)->times(50)->make();
         User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
 
-        $user = User::find(2);
+        $user = User::find(1);
         $user->name = 'luolei';
         $user->email = 'luolei1116@gmail.com';
         $user->password = bcrypt('luolei');
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
 Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
